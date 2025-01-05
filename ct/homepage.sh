fc803ad08905b2b1c8a7c1a7a201ff612683156c
Sébastiaan <se_bastiaan@outlook.com>
2025-01-05 14:19:20 +0100

    Change name because it is more than headers

--- ct/homepage.sh
+++ ct/homepage.sh
@@ -3,7 +3,7 @@ source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/m
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://gethomepage.dev/  
+# Source: https://gethomepage.dev/
 
 # App Default Values
 APP="Homepage"
